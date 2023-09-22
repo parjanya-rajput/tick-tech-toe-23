d35fdacbac521f4651541a9352eb06b0017252ef
Pranshu Patel <pranshu05patel@gmail.com>
2023-09-23 02:23:35 +0530

    homepage

---
+++ frontend/src/components/HomeCont.js
@@ -0,0 +1,7 @@
+export const HomeCont = () => {
+    return (
+        <div className="home-cont">
+            <img src="https://cdni.iconscout.com/illustration/premium/thumb/humanitarian-aid-3942206-3575518.png" alt=""/>
+        </div>
+    )
+}
